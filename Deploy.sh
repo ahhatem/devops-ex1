9539226c89a2ce530c6627afe2b7618e4e07d425
AhHatem <Ahhatem@gmail.com>
2021-10-05 17:13:55 +0200

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -4,4 +4,4 @@ basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo BaseDir is $basedir
 
 docker build -t terrafromansible ./src
-docker run -v ./src:/src -t terrafromansible /src/Deploy.sh
+docker run -v "$basedir/src":/src -t terrafromansible /src/Deploy.sh 
